c40840a0461d2ef1c1a6f6e39b3e426bcf602202
Aron Nopanen <anopanen@hdms.com>
2021-08-18 11:41:16 -0700

    Cleanup

--- sheep.js
+++ sheep.js
@@ -1,22 +1,12 @@
 const grid = document.getElementById('grid');
-// const NUM_COLS = Math.floor(window.innerWidth / 26);
-// const NUM_ROWS = Math.floor(window.innerHeight / 26);
 
-// const gridContainer = grid.parentElement;
-// const NUM_COLS = Math.floor(gridContainer.offsetWidth / 26);
-// const NUM_ROWS = Math.floor(gridContainer.offsetHeight / 26);
-
-let { width: gridWidth, height: gridHeight } = innerDimensions(grid);
 const CELL_SIZE = 56;
-// const NUM_COLS = Math.floor(gridWidth / CELL_SIZE);
-// const NUM_ROWS = Math.floor(gridHeight / CELL_SIZE);
 const NUM_COLS = 5;
 const NUM_ROWS = 5;
 const NUM_CELLS = NUM_ROWS * NUM_COLS;
 
 function initGrid() {
     grid.innerHTML = '';
-    // grid-template-columns: repeat(40, 25px);
     grid.style.setProperty("grid-template-columns",
         "repeat(" + NUM_COLS + ", " + CELL_SIZE + "px)");
     for (var idx = 0; idx < NUM_CELLS; idx++) {
@@ -27,22 +17,10 @@ function initGrid() {
     }
 }
 
-function createRandomModel() {
+function createModel() {
     const model = [];
     for (var idx = 0; idx < NUM_CELLS; idx++) {
-        // const alive = Math.random() > 0.7;
-        const alive = false;
-        model.push(alive);
-    }
-
-    return model;
-}
-
-function randomizeModel(model) {
-    for (var idx = 0; idx < NUM_CELLS; idx++) {
-        if (!model[idx]) {
-            model[idx] = Math.random() > 0.7;
-        }
+        model.push(false);
     }
 
     return model;
@@ -95,190 +73,23 @@ function seen(model, idx) {
                 return true;
             }
         }
-        // let offsetLeft = rowNum - rowIdx;
-        // if (offsetLeft <= colRoot) {
-        //     let cellId = (rowNum * NUM_COLS) + (colRoot - offsetLeft);
-        //     if (model[cellId]) {
-        //         return true;
-        //     }
-        // }
-        // let offsetRight = rowNum + rowIdx;
-        // if (offsetRight + colRoot < NUM_COLS) {
-        //     let cellId = (rowNum * NUM_COLS) + (colRoot + offsetRight);
-        //     if (model[cellId]) {
-        //         return true;
-        //     }
-        // }
     }
 
-    // let tlToBrRoot = idx % (NUM_COLS + 1);
-    // for (i = tlToBrRoot; i < NUM_CELLS; i += (NUM_COLS + 1)) {
-    //     if (model[i]) {
-    //         return true;
-    //     }
-    // }
-    // let trToBlRoot = idx % (NUM_COLS - 1);
-    // for (i = trToBlRoot; i < NUM_CELLS; i += (NUM_COLS - 1)) {
-    //     if (model[i]) {
-    //         return true;
-    //     }
-    // }
     return false;
 }
 
-function iterateModel(model) {
-    const nextModel = [];
-    for (var idx = 0; idx < NUM_CELLS; idx++) {
-        const numNeighbors = liveNeighbors(model, idx);
-        const currAlive = model[idx];
-        var nextAlive;
-        if (currAlive) {
-            nextAlive = numNeighbors === 2 || numNeighbors === 3;
-        } else {
-            nextAlive = numNeighbors === 3;
-        }
-        nextModel.push(nextAlive);
-    }
-    return nextModel;
-}
-
-function liveNeighbors(model, idx) {
-    var neighborIdcs = [];
-    // left
-    if (idx % NUM_COLS != 0) {
-        neighborIdcs.push(idx - 1);
-        neighborIdcs.push(idx - NUM_COLS - 1);
-        neighborIdcs.push(idx + NUM_COLS - 1);
-    }
-    // right
-    if (idx % NUM_COLS != NUM_COLS - 1) {
-        neighborIdcs.push(idx + 1);
-        neighborIdcs.push(idx - NUM_COLS + 1);
-        neighborIdcs.push(idx + NUM_COLS + 1);
-    }
-    // up
-    neighborIdcs.push(idx - NUM_COLS);
-    // down
-    neighborIdcs.push(idx + NUM_COLS);
-    neighborIdcs = neighborIdcs.filter(function(value) {
-        return value >= 0 && value < NUM_CELLS;
-    });
-    return neighborIdcs.reduce(function(count, curr) {
-        if (model[curr]) {
-            return count + 1;
-        } else {
-            return count;
-        }
-    }, 0);
-}
-
-function innerDimensions(node) {
-    var computedStyle = getComputedStyle(node);
-  
-    let width = node.clientWidth; // width with padding
-    let height = node.clientHeight; // height with padding
-  
-    height -= parseFloat(computedStyle.paddingTop) + parseFloat(computedStyle.paddingBottom);
-    width -= parseFloat(computedStyle.paddingLeft) + parseFloat(computedStyle.paddingRight);
-    return { height, width };
-}
-
 (function() {
-    var timer = null;
-    
-    // let b = document.getElementById("startStopButton");
-    // let rb = document.getElementById("randomizeButton");
-    // let sp = document.getElementById("speedSlider");
-    // let sd = document.getElementById("speedDisplay");
     let grid = document.getElementById("grid");
 
-    // let speed = sp.value;
-    let speed = 500;
-
-    var selecting = false;
-    var setting = false;
-
-    // b.addEventListener("click", function(event) {
-    //     if (timer == null) {
-    //         let speed = sp.value;
-    //         timer = window.setInterval(doGeneration, speed);
-    //         b.innerText = "Stop";
-    //         grid.classList.remove("stopped");
-    //     } else {
-    //         window.clearInterval(timer);
-    //         timer = null;
-    //         b.innerText = "Start";
-    //         grid.classList.add("stopped");
-    //     }
-    // });
-
-    // rb.addEventListener("click", function(event) {
-    //     randomizeModel(model);
-    //     renderModel(model);
-    // });
-
-    // sd.value = sp.value + " ms";
-    // sp.addEventListener("input", function(event) {
-    //     let speed = sp.value;
-    //     sd.value = speed + " ms";
-    //     if (timer) {
-    //         window.clearInterval(timer);
-    //         timer = window.setInterval(doGeneration, speed);
-    //     }
-    // });
-
     initGrid();
-    var model = createRandomModel();
+    var model = createModel();
     renderModel(model);
 
     let cells = document.getElementsByClassName("cell");
     for (let i=0, cell; (cell = cells[i]) !== undefined; i++) {
-        // cell.setAttribute("data-index", i);
         cell.addEventListener('click', function(event) {
             model[i] = !model[i];
             renderModel(model);
         });
-        // cell.addEventListener('mousedown', function(event) {
-        //     model[i] = !model[i];
-        //     renderModel(model);
-        // });
-        // cell.addEventListener("mouseover", function() {
-        //     if (selecting) {
-        //         model[i] = setting;
-        //         renderModel(model);
-        //     }
-        // });
     }
-
-    // grid.addEventListener('mousedown', function(event) {
-    //     console.log('mousedown');
-    //     selecting = true;
-    //     setting = event.button === 0; // Set on left mouse click
-    //     // if (event.target.hasAttribute("data-index")) {
-    //     //     console.log("Has data-index: " + event.target.getAttribute("data-index"));
-    //     //     setting = model[event.target.getAttribute("data-index")];
-    //     // } else {
-    //     //     setting = true;
-    //     // }
-    //     event.preventDefault();
-    // });
-
-    // grid.addEventListener('mouseup', function(event) {
-    //     selecting = false;
-    // });
-
-    // grid.addEventListener('contextmenu', function(event) {
-    //     event.preventDefault();
-    // });
-
-    // grid.addEventListener('dragstart', function(event) {
-    //     event.preventDefault();
-    // });
-
-    // function doGeneration() {
-    //     model = iterateModel(model);
-    //     renderModel(model);
-    // }
-    
-    // timer = window.setInterval(doGeneration, speed);
 })();
